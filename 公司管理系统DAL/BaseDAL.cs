b368cdcd2fca7af4d818d0160e48b46a90590eea
罗威 <3051755379@qq.com>
2019-06-18 16:56:39 +0800

    DAL层

--- 公司管理系统DAL/BaseDAL.cs
+++ 公司管理系统DAL/BaseDAL.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using 公司管理系统Model;
 
 namespace 公司管理系统DAL
 {
-   public class BaseDAL
+
+    public class BaseDAL<T> where T : class
     {
+        CompanyEntities db = new CompanyEntities();
+        public virtual List<T> GetAll()
+        {
+           
+
+            return db.Set<T>().ToList();
+        }
+        public virtual T GetByPK(object pk)
+        {
+       
+            return db.Set<T>().Find(pk);
+        }
+        public virtual bool Add(T info)
+        {
+            
+            db.Set<T>().Add(info);
+            return db.SaveChanges() > 0;
+        }
+        public virtual bool Delete(object pk)
+        {
+           
+            var info = db.Set<T>().Find(pk);
+            if (info != null)
+            {
+                db.Set<T>().Remove(info);
+                return db.SaveChanges() > 0;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        //条件查询
+        public virtual List<T> Where(Expression<Func<T, bool>> tj) {
+           
+            return db.Set<T>().Where(tj).ToList();
+        }
+        //
+        public virtual bool Update(T info)
+        {
+            
+            db.Entry<T>(info).State = System.Data.Entity.EntityState.Modified;
+            return db.SaveChanges() > 0;
+        }
+
     }
 }
---
+++ 公司管理系统DAL/actionTypeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 公司管理系统Model;
+
+namespace 公司管理系统DAL
+{
+   public class ActionTypeService:BaseDAL<actionType>
+    {
+    }
+}
