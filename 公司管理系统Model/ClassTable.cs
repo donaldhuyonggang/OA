5835006f33ed701893eda257d82e817088e6ffca
罗威 <3051755379@qq.com>
2019-06-19 14:22:13 +0800

    实体类

---
+++ 公司管理系统Model/ClassTable.cs
@@ -0,0 +1,34 @@
+namespace 公司管理系统Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("ClassTable")]
+    public partial class ClassTable
+    {
+        [Key]
+        public int ClassId { get; set; }
+
+        [StringLength(20)]
+        public string Morning { get; set; }
+
+        [StringLength(20)]
+        public string Afertnoon { get; set; }
+
+        public int? UserIde { get; set; }
+
+        public int? UserId { get; set; }
+
+        public DateTime? DataTime { get; set; }
+
+        [StringLength(50)]
+        public string require { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual User User1 { get; set; }
+    }
+}
