e17f6ec01e8ec9472b0027fb774affea61e7354c
罗威 <3051755379@qq.com>
2019-06-20 14:14:23 +0800

    UI

---
+++ 公司管理系统UI/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 公司管理系统UI.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
