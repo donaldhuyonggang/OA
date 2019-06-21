e533239fba3810725fa53bff5d9485c19dba3e73
凯 唐 <1935785731@qq.com>
2019-06-21 10:55:17 +0800

    后台母版页样式，以及英文的修改

---
+++ 公司管理系统UI/Areas/TangKai/Controllers/UpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 公司管理系统UI.Areas.TangKai.Controllers
+{
+    public class UpController : Controller
+    {
+        // GET: TangKai/Up
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
