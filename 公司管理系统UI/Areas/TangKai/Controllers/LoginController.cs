989814344a35fc6967595ca9b3788bf873ee3f8b
凯 唐 <1935785731@qq.com>
2019-06-21 09:07:58 +0800

    增加了后台登陆界面

---
+++ 公司管理系统UI/Areas/TangKai/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 公司管理系统UI.Areas.TangKai.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: TangKai/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
