f3d653f89fd2eff07f9637622d7cd3f777379cc1
773141653 <773141653@qq.com>
2019-06-23 14:26:45 +0800

    123

--- 公司管理系统UI/App_Start/RightAttribute.cs
+++ 公司管理系统UI/App_Start/RightAttribute.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using 公司管理系统BLL;
 using 公司管理系统Model;
 
 namespace 公司管理系统UI.App_Start
@@ -15,8 +16,33 @@ namespace 公司管理系统UI.App_Start
             User Users= filterContext.HttpContext.Session["User"] as User;
             if (Users==null) {
 
-
             }
+            else
+            {
+                if (Users.AdminId == "True")
+                {
+                    return;
+                }
+
+                /*如果不是超级管理员，就获取判断是否有权限访问，*/
+                string controller = filterContext.RouteData.Values["controller"].ToString();
+                string action = filterContext.RouteData.Values["action"].ToString();
+                /*判断要访问的路径，时候跟权限路劲一样*/
+                string Url = "/" + controller + "/" + action;
+                Sys_UserRoleManager Sy = new Sys_UserRoleManager();
+               
+                foreach (var item in Users.Sys_Role)
+                {
+                    foreach (var i in item.Sys_Menu)
+                    {
+                        if (i.MenuPath== Url) {
+                            return;
+                        }
+
+                          }
+                    }
+                }
+            filterContext.HttpContext.Response.Redirect("~/Home/Index");
         }
         /*执行后*/
         public void OnActionExecuting(ActionExecutingContext filterContext)
