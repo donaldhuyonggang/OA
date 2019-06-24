cf49fd4c82289efd8cb43514d89ba65015a7c22e
773141653 <773141653@qq.com>
2019-06-24 10:58:38 +0800

    123

--- 公司管理系统DAL/Sys_RightServer.cs
+++ 公司管理系统DAL/Sys_RightServer.cs
@@ -7,7 +7,7 @@ using 公司管理系统Model;
 
 namespace 公司管理系统DAL
 {
-  public  class Sys_RightServer:BaseDAL<Sys_Right>
+  public  class Sys_RightServer
     {
     }
 }
--- 公司管理系统DAL/web_typeService.cs
+++ 公司管理系统DAL/web_typeService.cs
@@ -7,7 +7,7 @@ using 公司管理系统Model;
 
 namespace 公司管理系统DAL
 {
-   public class Sys_UserRoleServer
+   public class web_typeService:BaseDAL<web_type>
     {
     }
 }
