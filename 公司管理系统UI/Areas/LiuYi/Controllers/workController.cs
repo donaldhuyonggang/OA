6d2ffdc0c4f6a2e8c130bb730f87640a26befca6
22404 <22404@DESKTOP-OQKCFD1>
2019-07-01 12:11:50 +0800

    刘某

---
+++ 公司管理系统UI/Areas/LiuYi/Controllers/workController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using 公司管理系统BLL;
+using 公司管理系统Model;
+
+namespace 公司管理系统UI.Areas.LiuYi.Controllers
+{
+    public class workController : Controller
+    {
+        /// <summary>
+        /// 前台课表查询
+        /// </summary>
+        /// <returns></returns>
+        // GET: LiuYi/work
+        public ActionResult Index()
+        {
+            ClassManager bll = new ClassManager();
+            List<_class> list = bll.Where(x=>x.class_Condition=="可见");
+            return View(list);
+        }
+    }
+}
